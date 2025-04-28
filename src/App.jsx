480b3c1d7fac7bc00bc76e463c932a2ecc73f47e
ThetHtetS <thethtetsoe114@gmail.com>
2025-04-28 10:39:11 +0630

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,42 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Logo from './assets/icon.png'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+
+    const handleDownload = () => {
+      const downloadUrl = "https://firebasestorage.googleapis.com/v0/b/seventri-fd94d.firebasestorage.app/o/application-ee7da1f8-1c68-4752-a26b-9ddfae72294f.apk?alt=media&token=7989d9df-c001-49e0-b420-8e1efebc441d";
+  
+      const link = document.createElement('a');
+      link.href = downloadUrl;
+      link.download = 'your-file.jpg'; // You can set the file name here
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    };
+  
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {/* <a href="https://vite.dev" target="_blank"> */}
+          <img src={Logo} className="logo" alt="Vite logo" />
+        {/* </a> */}
+        
       </div>
-      <h1>Vite + React</h1>
+      <h1>BKK 6D</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={handleDownload}>
+           Download 
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          {/* Edit <code>src/App.jsx</code> and save to test HMR */}
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click Download button to download android apk
       </p>
     </>
   )
